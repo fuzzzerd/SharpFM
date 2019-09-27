ad1e73194557219ffd093029fa066c79280c0fc8
Nate Bross <nate@brosstribe.com>
2019-09-27 10:56:48 -0500

    updated type mapping

--- SharpFM.Core/LayoutAsFMDataModel.cs
+++ SharpFM.Core/LayoutAsFMDataModel.cs
@@ -28,57 +28,42 @@ namespace SharpFM.Core
             // Add System using statement: (using System)
             @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
 
-            //  Create a class: (class Order)
+            //  Create a class: (class [_clip.Name])
             var classDeclaration = SyntaxFactory.ClassDeclaration(_clip.Name);
 
-            // Add the public modifier: (public class Order)
+            // Add the public modifier: (public class [_clip.Name])
             classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
 
-            //// Inherit BaseEntity<T> and implement IHaveIdentity: (public class Order : BaseEntity<T>, IHaveIdentity)
-            //classDeclaration = classDeclaration.AddBaseListTypes(
-            //    SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("BaseEntity<Order>")),
-            //    SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("IHaveIdentity")));
-
-            // Create a string variable: (bool canceled;)
-            var variableDeclaration = SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("bool"))
-                .AddVariables(SyntaxFactory.VariableDeclarator("canceled"));
-
-            // Create a field declaration: (private bool canceled;)
-            var fieldDeclaration = SyntaxFactory.FieldDeclaration(variableDeclaration)
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PrivateKeyword));
-
+            // add each field from the underling _clip as a public property with the data member attribute
             List<PropertyDeclarationSyntax> fieldsToBeAddedAsProperties = new List<PropertyDeclarationSyntax>(_clip.Fields.Count());
             foreach(var field in _clip.Fields)
             {
-                var propertyTypeCSharp = field.DataType.Replace("Text", "string").Replace("Number", "int");
-                var propertyTypeSyntax = SyntaxFactory.ParseTypeName("int");
+                // filemaker to C# data type mapping
+                var propertyTypeCSharp = field.DataType
+                    .Replace("Text", "string")
+                    .Replace("Number", "int")
+                    .Replace("Binary", "byte[]")
+                    .Replace("Date", "DateTime")
+                    .Replace("Time", "TimeStamp")
+                    .Replace("TimeStamp", "DateTime");
+
+                var propertyTypeSyntax = SyntaxFactory.ParseTypeName(propertyTypeCSharp);
+
+                var dataMemberAttribute = SyntaxFactory.Attribute(SyntaxFactory.ParseName("DataMember"));
 
                 var propertyDeclaration = SyntaxFactory.PropertyDeclaration(propertyTypeSyntax, field.Name)
                 .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
                 .AddAccessorListAccessors(
                     SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
-                    SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
-                
+                    SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)))
+                .NormalizeWhitespace()
+                .AddAttributeLists(SyntaxFactory.AttributeList(SyntaxFactory.SingletonSeparatedList(dataMemberAttribute)));
+
                 fieldsToBeAddedAsProperties.Add(propertyDeclaration);
             }
 
-            //// Create a Property: (public int Quantity { get; set; })
-            //var propertyDeclaration = SyntaxFactory.PropertyDeclaration(SyntaxFactory.ParseTypeName("int"), "Quantity")
-            //    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-            //    .AddAccessorListAccessors(
-            //        SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)),
-            //        SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration).WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken)));
-
-            // Create a stament with the body of a method.
-            var syntax = SyntaxFactory.ParseStatement("canceled = true;");
-
-            // Create a method
-            var methodDeclaration = SyntaxFactory.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "MarkAsCanceled")
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .WithBody(SyntaxFactory.Block(syntax));
-
             // Add the field, the property and method to the class.
-            classDeclaration = classDeclaration.AddMembers(fieldDeclaration, methodDeclaration).AddMembers(fieldsToBeAddedAsProperties.ToArray());
+            classDeclaration = classDeclaration.AddMembers(fieldsToBeAddedAsProperties.ToArray());
 
             // Add the class to the namespace.
             @namespace = @namespace.AddMembers(classDeclaration);
