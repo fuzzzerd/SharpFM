144e28bb4e8e8835fef8f2e40ab5e739041996ae
Nate Bross <nate@brosstribe.com>
2023-09-28 21:23:18 -0500

    xml docs for dbcontext

--- SharpFM.App/Models/ClipDbContext.cs
+++ SharpFM.App/Models/ClipDbContext.cs
@@ -1,18 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 
+/// <summary>
+/// Clip Db Context
+/// </summary>
 public class ClipDbContext : DbContext
 {
-    public DbSet<Clip> Clips { get; set; }
+    /// <summary>
+    /// Clips stored in the Db.
+    /// </summary>
+    public DbSet<Clip> Clips => Set<Clip>();
 
+    /// <summary>
+    /// Database path.
+    /// </summary>
     public string DbPath { get; }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
     public ClipDbContext()
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "blogging.db");
+        DbPath = System.IO.Path.Join(path, "sharpFM.db");
     }
 
     // The following configures EF to create a Sqlite database file in the
@@ -21,10 +32,28 @@ public class ClipDbContext : DbContext
         => options.UseSqlite($"Data Source={DbPath}");
 }
 
+/// <summary>
+/// Clip Data Model
+/// </summary>
 public class Clip
 {
+    /// <summary>
+    /// Database Id
+    /// </summary>
     public int ClipId { get; set; }
+
+    /// <summary>
+    /// Display name for clip may match Name inside the xml data or may not.
+    /// </summary>
     public string ClipName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The data format to use when putting the data back on the clipboard for FileMaker.
+    /// </summary>
     public string ClipType { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Raw xml data from the clip.
+    /// </summary>
     public string ClipXml { get; set; } = string.Empty;
 }
