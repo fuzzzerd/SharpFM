6ebd3ca09de2ade485f37be3e037b4ffc4a9ebd2
Nate Bross <nate@brosstribe.com>
2023-09-29 12:39:51 -0500

    remove unused usings

--- SharpFM.App/ViewModels/MainWindowViewModel.cs
+++ SharpFM.App/ViewModels/MainWindowViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
