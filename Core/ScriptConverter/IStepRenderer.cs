7010330de33c0e71ff93fdf5686eb71970413148
Nate Bross <nate@brosstribe.com>
2026-03-29 23:52:51 -0500

    refactor: remove old string-based converter and renderer infrastructure

--- Core/ScriptConverter/IStepRenderer.cs
+++
@@ -1,9 +0,0 @@
-using System.Xml.Linq;
-
-namespace SharpFM.Core.ScriptConverter;
-
-public interface IStepRenderer
-{
-    string ToHr(XElement step, StepDefinition definition);
-    string ToXml(ParsedLine line, StepDefinition definition);
-}
--- Core/ScriptConverter/Renderers/IMultiStepRenderer.cs
+++
@@ -1,6 +0,0 @@
-namespace SharpFM.Core.ScriptConverter.Renderers;
-
-public interface IMultiStepRenderer : IStepRenderer
-{
-    string[] StepNames { get; }
-}
