aece29290d7abba68514ab71b8e831caa556fcae
Nate Bross <nate@brosstribe.com>
2026-03-30 20:57:11 -0500

    fix: restore Command bindings on table editor buttons, remove Click handler workaround

--- src/SharpFM/Schema/Editor/TableEditorControl.axaml.cs
+++ src/SharpFM/Schema/Editor/TableEditorControl.axaml.cs
@@ -1,7 +1,6 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Interactivity;
 using SharpFM.Schema.Model;
 
 namespace SharpFM.Schema.Editor;
@@ -14,23 +13,12 @@ public partial class TableEditorControl : UserControl
     public TableEditorControl()
     {
         InitializeComponent();
-
-        var addBtn = this.FindControl<Button>("addFieldButton");
-        var removeBtn = this.FindControl<Button>("removeFieldButton");
-        var calcBtn = this.FindControl<Button>("editCalcButton");
-
-        if (addBtn != null) addBtn.Click += (_, _) => GetVm()?.AddField();
-        if (removeBtn != null) removeBtn.Click += (_, _) => GetVm()?.RemoveSelectedField();
-        if (calcBtn != null) calcBtn.Click += (_, _) => GetVm()?.OpenCalculationEditor();
-
         KeyDown += OnKeyDown;
     }
 
-    private TableEditorViewModel? GetVm() => DataContext as TableEditorViewModel;
-
     private void OnKeyDown(object? sender, KeyEventArgs e)
     {
-        if (e.Key == Key.Delete && GetVm() is { } vm)
+        if (e.Key == Key.Delete && DataContext is TableEditorViewModel vm)
         {
             vm.RemoveSelectedField();
             e.Handled = true;
