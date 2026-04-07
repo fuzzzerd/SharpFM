e09df7a50c0e34a35ea7c16c1e266003d71885a3
Nate Bross <nate@brosstribe.com>
2026-04-07 12:56:42 -0500

    refactor: move operation types to SharpFM.Model

--- src/SharpFM.Model/Schema/FieldOperation.cs
+++ src/SharpFM.Model/Schema/FieldOperation.cs
@@ -1,9 +1,4 @@
-// This file is part of SharpFM and is licensed under the GNU General Public License v3.
-//
-// Plugin Exception: You may create plugins that implement these interfaces without those
-// plugins being subject to the GPL. Such plugins may use any license, including proprietary.
-
-namespace SharpFM.Plugin;
+namespace SharpFM.Model.Schema;
 
 /// <summary>
 /// A single field mutation within a batch update to a table clip.
--- src/SharpFM.Model/Scripting/ScriptStepOperation.cs
+++ src/SharpFM.Model/Scripting/ScriptStepOperation.cs
@@ -1,11 +1,6 @@
-// This file is part of SharpFM and is licensed under the GNU General Public License v3.
-//
-// Plugin Exception: You may create plugins that implement these interfaces without those
-// plugins being subject to the GPL. Such plugins may use any license, including proprietary.
-
 using System.Collections.Generic;
 
-namespace SharpFM.Plugin;
+namespace SharpFM.Model.Scripting;
 
 /// <summary>
 /// A single script step mutation within a batch update.
